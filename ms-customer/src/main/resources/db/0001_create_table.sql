6bb111c2f5dd21cb14088499e9075d9da4830331
Rony Villanueva <ronyvillame@gmail.com>
2024-11-04 08:06:53 -0500

    DB scripts

---
+++ ms-customer/src/main/resources/db/0001_create_table.sql
@@ -0,0 +1,15 @@
+create table customer (
+	id int primary key,
+	full_name varchar(100) not null,
+	gender varchar(2) not null,
+	age int not null,
+	identification varchar(20) not null,
+	address varchar(200) not null,
+    phone varchar(20) not null,
+	active boolean not null,
+	client_id varchar(50) not null,
+	password varchar(50) not null,
+	status varchar(3) not null
+)
+
+
